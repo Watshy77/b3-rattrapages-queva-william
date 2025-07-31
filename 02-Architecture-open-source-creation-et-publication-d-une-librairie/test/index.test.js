a0daf21af25f95f0c0046f7a6c38bd73e93c3538
William Queva <watshy77@gmail.com>
2025-07-31 21:01:04 +0200

    add test for variable, number and boolean

---
+++ 02-Architecture-open-source-creation-et-publication-d-une-librairie/test/index.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const { validateEnv } = require('../src/index');
+
+describe('validateEnv', () => {
+    beforeEach(() => {
+        delete process.env.TEST_VAR;
+        delete process.env.NUM_VAR;
+        delete process.env.BOOL_VAR;
+    });
+
+    it('should throw an error if a required variable is missing', () => {
+        const schema = [{ name: 'TEST_VAR', required: true, type: 'string' }];
+        expect(() => validateEnv(schema)).to.throw('Missing required env var: TEST_VAR');
+    });
+
+    it('should throw an error if the number type is invalid', () => {
+        process.env.NUM_VAR = 'not-a-number';
+        const schema = [{ name: 'NUM_VAR', required: true, type: 'number' }];
+        expect(() => validateEnv(schema)).to.throw('Env var NUM_VAR is not a valid number');
+    });
+
+    it('should throw an error if the boolean type is invalid', () => {
+        process.env.BOOL_VAR = 'maybe';
+        const schema = [{ name: 'BOOL_VAR', required: true, type: 'boolean' }];
+        expect(() => validateEnv(schema)).to.throw('Env var BOOL_VAR is not a valid boolean');
+    });
+});
