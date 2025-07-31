ac2d141a4dd1d5a68bc995ca5b6c3f23f3ad9d4d
William Queva <watshy77@gmail.com>
2025-07-31 20:55:55 +0200

    add function validateEnv

---
+++ 02-Architecture-open-source-creation-et-publication-d-une-librairie/src/index.js
@@ -0,0 +1,38 @@
+/**
+ * validateEnv(schema)
+ * @param {Array<{name: string, required: boolean, type: string, regex?: string}>} schema
+ */
+function validateEnv(schema) {
+    schema.forEach(def => {
+        const val = process.env[def.name];
+        if (def.required && (val === undefined || val === '')) {
+            throw new Error(`Missing required env var: ${def.name}`);
+        }
+        if (val !== undefined && val !== '') {
+            switch (def.type) {
+                case 'number':
+                    if (isNaN(Number(val))) {
+                        throw new Error(`Env var ${def.name} is not a valid number`);
+                    }
+                    break;
+                case 'boolean':
+                    if (!['true', 'false'].includes(val.toLowerCase())) {
+                        throw new Error(`Env var ${def.name} is not a valid boolean`);
+                    }
+                    break;
+                case 'string':
+                    break;
+                default:
+                    throw new Error(`Unknown type for env var ${def.name}: ${def.type}`);
+            }
+            if (def.regex) {
+                const re = new RegExp(def.regex);
+                if (!re.test(val)) {
+                    throw new Error(`Env var ${def.name} does not match pattern ${def.regex}`);
+                }
+            }
+        }
+    });
+}
+
+module.exports = { validateEnv };
